983bb57aeb0484659791b54a3d2e57d58073fe8c
Braian Emanuel Lopez <braian.lopez.ze@outlook.com>
2020-09-27 04:37:59 -0300

    Updates

---
+++ precache-manifest.476c159ff3c42b374cde048080685889.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b60d09848ca4ecf194e8345600c33d6b",
+    "url": "/Lyts-Client/index.html"
+  },
+  {
+    "revision": "ccaa93eedd3a96b9363f",
+    "url": "/Lyts-Client/static/css/main.cdd9e698.chunk.css"
+  },
+  {
+    "revision": "ff40b449236bf27b7704",
+    "url": "/Lyts-Client/static/js/2.c4473a2a.chunk.js"
+  },
+  {
+    "revision": "89909ad3899b46f7e79ffd5842c0dfa2",
+    "url": "/Lyts-Client/static/js/2.c4473a2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccaa93eedd3a96b9363f",
+    "url": "/Lyts-Client/static/js/main.b7b8be62.chunk.js"
+  },
+  {
+    "revision": "1f54bb2ed4e99d18385c",
+    "url": "/Lyts-Client/static/js/runtime-main.c3d850fc.js"
+  }
+]);
